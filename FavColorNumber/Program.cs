2c7bf9abdf446d1bf04f7afe447ceb2e317b35d1
Gage Hubler <ghubler748@mynccc.niagaracc.suny.edu>
2020-08-26 20:23:44 -0400

    Initial upload of project file

---
+++ FavColorNumber/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FavColorNumber
+{
+    // Class:IGME-206
+    // Author: Gage Hubler
+    // Purpose: Repeat back favorite color in the amount of 
+    //  their favorite number
+    // Restrictions: None
+    class Program
+    {
+        // Method: Main
+        // Purpose: Prompt the user for their favorite color and number
+        //          Output their favorite color (in limited text colors) their
+        //           their favorite number of times
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ SquashTheBugs/SquashTheBugs/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace SquashTheBugs
+{
+    // Class Program
+    // Author: David Schuh
+    // Purpose: Bug squashing exercise
+    // Restrictions: None
+    class Program
+    {
+        // Method: Main
+        // Purpose: Loop through the numbers 1 through 10 
+        //          Output N/(N-1) for all 10 numbers
+        //          and list all numbers processed
+        // Restrictions: None
+        static void Main(string[] args)
+        {
+            // declare int counter
+            // int i = 0
+            // Compile-time error: Missing semicolon
+            int i = 0;
+
+            // declare string to hold all numbers
+            string allNumbers = null;
+
+            // loop through the numbers 1 through 10
+            // Logic error: loop should iterate 1-10 instead of 1-9
+            // for (i = 1; i < 10; ++i)
+            for (i = 1; i <= 10; ++i)
+            {
+
+                // declare string to hold all numbers
+                // Logic error: Variable initialized inside for loop not useable outside
+                // string allNumbers = null;
+
+                // output explanation of calculation
+                // Compile time error: statement not interpolated correctly
+                // Console.Write(i + "/" + i - 1 + " = ");
+                Console.Write($"{ i }" + "/" + $"{ i - 1 }" + " = ");
+
+                // output the calculation based on the numbers
+                // Run-time error: division by zero possible
+                // Logic error: Doesn't print as a double
+                // Console.WriteLine(i / (i - 1));
+                if (i == 1)
+                {
+                    Console.WriteLine("Undefined");
+                }
+                else
+                {
+                    Console.WriteLine(Convert.ToDouble(i) / (i - 1));
+                }
+
+                // concatenate each number to allNumbers
+                allNumbers += i + " ";
+
+                // increment the counter
+                // Logic error: counter in for-loop body unnecessary 
+                // i = i + 1;
+            }
+
+            // output all numbers which have been processed
+            // Console.WriteLine("These numbers have been processed: " allNumbers);
+            // Compile-time error: statement not concatenated correctly
+            Console.WriteLine($"These numbers have been processed: {allNumbers}");
+        }
+    }
+}
