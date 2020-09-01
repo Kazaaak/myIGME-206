59e2857cf5679e40e35261c8c3f35454eee54703
Gage Hubler <ghubler748@mynccc.niagaracc.suny.edu>
2020-08-31 22:44:25 -0400

    Upload for question 2 of PE4

---
+++ PE3Q5Hubler/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE3Question5Hubler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input1, input2, input3, input4;
+            int num1 = 0, num2 = 0, num3 = 0, num4 = 0;
+
+            // Prompt the user for each value, then store as a string.
+            Console.Write("Please enter your first integer: ");
+            input1 = Console.ReadLine();
+
+            Console.Write("Please enter your second integer: ");
+            input2 = Console.ReadLine();
+
+            Console.Write("Please enter your third integer: ");
+            input3 = Console.ReadLine();
+
+            Console.Write("Please enter your fourth integer: ");
+            input4 = Console.ReadLine();
+
+            // Convert the inputs into integers
+            num1 = Convert.ToInt32(input1);
+            num2 = Convert.ToInt32(input2);
+            num3 = Convert.ToInt32(input3);
+            num4 = Convert.ToInt32(input4);
+
+            Console.WriteLine($"\nThe product of these numbers is: {num1 * num2 * num3 * num4}\n");
+        }
+    }
+}
---
+++ PE4-Q2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE4_Q2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string var1;
+            string var2;
+            int num1 = 0; ;
+            int num2 = 0;
+
+            // Repeat this statement while numbers are not valid
+            do
+            {
+                // Request and record both integers from user
+                Console.Write("Please enter an integer: ");
+                var1 = Console.ReadLine();
+                num1 = Convert.ToInt32(var1);
+
+                Console.Write("Please enter another integer: ");
+                var2 = Console.ReadLine();
+                num2 = Convert.ToInt32(var2);
+
+            // Repeat the looped code until only one number is > 10
+            } while ((num1 > 10 && num2 > 10) || (!(num1 > 10) && !(num2 > 10)));
+            
+            // Indicate to the user that the code has successfully completed
+            Console.WriteLine("Numbers are valid, thank you!");
+        }
+    }
+}
