77554070f57661ee21d7eb35cd3b2e615a69505c
Gage Hubler <ghubler748@mynccc.niagaracc.suny.edu>
2020-09-16 20:31:00 -0400

    Added T1-Q3

---
+++ T1-Q3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T1_Q3
+{
+    class Program
+    {
+        delegate string stringMethod(string input);
+
+        
+        static void Main(string[] args)
+        {
+            string input;
+            Console.Write("Please enter a string: ");
+            input = new stringMethod(ReadInput);
+            Console.WriteLine(input);
+        }
+
+        static string ReadInput(string input)
+        {
+            input = Console.ReadLine();
+            return input;
+        }
+    }
+}
+
+
+
+
+// Class: IGME-206
+// Author: Gage Hubler
+// Purpose: Uses a delegate to impersonate the Console.ReadLine() function when asking for user input
+// Restrictions: None
