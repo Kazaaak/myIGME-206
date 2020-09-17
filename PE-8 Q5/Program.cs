3a6419111fddca115c9744182634ed3b16e6adef
Gage Hubler <ghubler748@mynccc.niagaracc.suny.edu>
2020-09-16 22:03:33 -0400

    Added T1-Q12

--- PE-8 Q5/Program.cs
+++ PE-8 Q5/Program.cs
@@ -42,3 +42,4 @@ namespace PE_8_Q5
         }
     }
 }
+
---
+++ T1-Q12/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T1_Q12
+{
+    class Program
+    {
+        // Class: IGME-206
+        // Author: Gage Hubler
+        // Purpose: Increases the user's salary and congratulates them if they have my name
+        // Restrictions: None
+        static void Main(string[] args)
+        {
+            string sName;
+            double dSalary = 30000;
+            bool bRaise = false;
+
+            Console.WriteLine("Please enter your name: ");
+            sName = Console.ReadLine();
+
+            bRaise = GiveRaise(sName, ref dSalary);
+
+            if (bRaise == true)
+            {
+                Console.WriteLine("Congratulations on your raise, " + sName + ". Your new salary is: " + dSalary + "!");
+            }
+            
+        }
+
+        static bool GiveRaise(string name, ref double salary)
+        {
+            if (name == "Gage") {
+                salary += 19999.99;
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+    }
+}
